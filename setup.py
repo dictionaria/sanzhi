d10f9686f30f6937919f49daa1e79b0ca6fd8a77
Johannes Englisch <englisch@shh.mpg.de>
2021-02-08 13:18:19 +0100

    cldfbench

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+setup(
+    name='cldfbench_sanzhi',
+    py_modules=['cldfbench_sanzhi'],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        'cldfbench.dataset': [
+            'sanzhi=cldfbench_sanzhi:Dataset',
+        ]
+    },
+    install_requires=[
+        'cldfbench',
+    ],
+    extras_require={
+        'test': [
+            'pytest-cldf',
+        ],
+    },
+)
